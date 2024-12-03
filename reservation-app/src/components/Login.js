f3cf33d70d11e996aae6dd2c502774398163a898
澤井翁 <hello@iwasanaoki.com>
2024-12-03 12:35:12 +0900

    依存関係を整理

--- reservation-app/src/components/Login.js
+++ reservation-app/src/components/Login.js
@@ -14,7 +14,7 @@ const Login = ({ setToken }) => {
         e.preventDefault();
         setError(''); // エラーメッセージのリセット
         try {
-            const response = await axios.post('http://localhost:3000/api/auth/login', { username, password });
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, { username, password });
             const token = response.data.token;
 
             // トークンをローカルストレージに保存して状態を更新
